4ae55f97a21664fcca35954bbfbda8726640889b
Pedro <pierre.beaudraps@gmail.com>
2018-09-11 10:22:51 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,20 +9,36 @@
                 <div class="page-content">
                     <h2>Register</h2>
                     <div class="form-style form-style-3">
-                        <form>
+                        <form method="POST" action="{{ route('register') }}">
+                            {{ csrf_field() }}
 
                             <div class="form-inputs clearfix">
                                 <p class="login-text">
-                                    <input type="text" name="name" value="" placeholder="Username" required autofocus>
+                                    <input type="text" name="name" value="{{ old('name') }}" placeholder="Username" required autofocus>
                                     <i class="icon-user"></i>
+                                    @if ($errors->has('name'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                    @endif
                                 </p>
                                 <p class="login-text">
-                                    <input type="email" name="email" value="" placeholder="Email" required autofocus>
+                                    <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
                                     <i class="icon-envelope"></i>
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
                                 </p>
                                 <p class="login-password">
                                     <input type="password" name="password" placeholder="Password" required>
                                     <i class="icon-lock"></i>
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
                                 </p>
                                 <p class="login-password">
                                     <input id="password-confirm" type="password" name="password_confirmation" placeholder="Repeat password" required>
