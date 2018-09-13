ce49fa44bf73565e90685cd5413d6086de12ed35
Pedro <pierre.beaudraps@gmail.com>
2018-09-13 10:48:45 +0200

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreQuestion;
+use App\Http\Controllers\Controller;
+use App\Repositories\QuestionRepository;
+
+class HomeController extends Controller
+{
+    protected $questionRepository;
+
+
+    public function __construct(QuestionRepository $questionRepository)
+    {
+        $this->questionRepository = $questionRepository;
+    }
+
+    /**
+     *
+     */
+    public function index()
+    {
+        $questions = $this->questionRepository->getOrdered();
+        $recentQuestions = $this->questionRepository->getRecent(2);
+
+
+        return view('homepage', compact('questions', 'recentQuestions'));
+    }
+
+}
---
+++ app/Http/Controllers/UpvoteController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpvote;
+use App\Http\Controllers\Controller;
+use App\Repositories\UpvoteRepository;
+
+class UpvoteController extends Controller
+{
+
+    protected $upvoteRepository;
+
+    public function __construct(UpvoteRepository $upvoteRepository)
+    {
+        $this->upvoteRepository = $upvoteRepository;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreUpvote $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(StoreUpvote $request)
+    {
+        
+        $requestData = $request->all();
+        
+        $this->upvoteRepository->create($requestData);
+
+        return redirect()->route('questions.show', ['id' => $request->get('question_id')]);
+    }
+
+
+}
